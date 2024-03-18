4d02a574e63ca48ff71e507802568f835217ccc6
guidarmike <guidara.michele@gmail.com>
2024-03-18 09:40:12 +0100

    Prophets update

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -2,10 +2,16 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 const cards = document.querySelector('#cards');
 
 async function getProphetData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.table(data.prophets); 
-    displayProphets(data.prophets);
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        displayProphets(data.prophets);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 getProphetData();
@@ -14,6 +20,7 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
+        let birthInfo = document.createElement('p'); 
         let portrait = document.createElement('img');
 
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
@@ -23,7 +30,10 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
+        birthInfo.textContent = `Birthplace: ${prophet.birthplace}, Birthdate: ${prophet.birthdate}`;
+
         card.appendChild(fullName);
+        card.appendChild(birthInfo); 
         card.appendChild(portrait);
 
         cards.appendChild(card);
