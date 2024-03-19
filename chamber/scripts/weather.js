8c251da25787501896e7c96af2f3a110d40bed61
guidarmike <guidara.michele@gmail.com>
2024-03-19 19:46:48 +0100

    Update Weather Chamber

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -108,8 +108,10 @@ function mode(arr) {
 displayWeather();
 
 const today = new Date().getDay();
-if (today >= 1 && today <= 3) {
+if (today === 1 || today === 2 || today === 3) {
   banner.style.display = 'block';
+} else {
+  banner.style.display = 'none';
 }
 
 closeButton.addEventListener('click', () => {
